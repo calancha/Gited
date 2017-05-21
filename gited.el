9b535108adc74cd4d2834b8431fd6eac6c98b9be
Tino Calancha <tino.calancha@gmail.com>
2017-05-21 14:46:31 +0900

    Increase Emacs minimum version support to 24.4
    Current implementation uses macro 'with-current-buffer-window'
    which was introduced in Emacs 24.4.

--- gited.el
+++ gited.el
@@ -7,12 +7,12 @@
 ;; Maintainer: Tino Calancha <tino.calancha@gmail.com>
 ;; Copyright (C) 2016-2017, Tino Calancha, all rights reserved.
 ;; Created: Wed Oct 26 01:28:54 JST 2016
-;; Compatibility: GNU Emacs: 24.3
+;; Compatibility: GNU Emacs: 24.4
 ;; Version: 0.1
-;; Package-Requires: ((emacs "24.3") (cl-lib "0.5"))
-;; Last-Updated: Sun May 21 14:38:05 JST 2017
+;; Package-Requires: ((emacs "24.4") (cl-lib "0.5"))
+;; Last-Updated: Sun May 21 14:43:41 JST 2017
 ;;           By: calancha
-;;     Update #: 596
+;;     Update #: 597
 ;;
 ;; Features that might be required by this library:
 ;;
