9791e61619dfbdaabddba45697f4dbe92ad5d3a3
Tino Calancha <tino.calancha@gmail.com>
2018-04-10 11:30:14 +0900

    * gited.el (gited-pull): Delete unused variable

--- gited.el
+++ gited.el
@@ -11,9 +11,9 @@
 ;; Compatibility: GNU Emacs: 24.4
 ;; Version: 0.4.3
 ;; Package-Requires: ((emacs "24.4") (cl-lib "0.5"))
-;; Last-Updated: Mon Apr 09 20:53:32 JST 2018
+;; Last-Updated: Tue Apr 10 11:26:35 JST 2018
 ;;           By: calancha
-;;     Update #: 683
+;;     Update #: 684
 ;;
 ;; Features that might be required by this library:
 ;;
@@ -2077,20 +2077,19 @@ show similar info as that command."
 (defun gited-pull ()
   "Pull from current remote repository."
   (interactive)
-  (let ((branch gited-current-branch))
-    (if (not (or gited-expert
-                 (y-or-n-p (format "Pull from remote repository '%s'? "
-                                   gited-current-remote-rep))))
-        (message "OK, pull canceled")
-      (let ((buf (gited--output-buffer))
-            (cmd (format "%s pull %s"
-                         vc-git-program
-                         gited-current-remote-rep))
-            (inhibit-read-only t))
-        (setq gited-output-buffer buf
-              gited-op-string cmd)
-        (with-current-buffer buf (erase-buffer))
-        (gited-async-operation cmd 'remote-op-p)))))
+  (if (not (or gited-expert
+               (y-or-n-p (format "Pull from remote repository '%s'? "
+                                 gited-current-remote-rep))))
+      (message "OK, pull canceled")
+    (let ((buf (gited--output-buffer))
+          (cmd (format "%s pull %s"
+                       vc-git-program
+                       gited-current-remote-rep))
+          (inhibit-read-only t))
+      (setq gited-output-buffer buf
+            gited-op-string cmd)
+      (with-current-buffer buf (erase-buffer))
+      (gited-async-operation cmd 'remote-op-p))))
 
 (defun gited-push (&optional force-with-lease)
   "Run git push in current branch.
