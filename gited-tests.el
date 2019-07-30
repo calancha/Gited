f24316407c32e4003f789edcdac894adfcc4fa03
Tino Calancha <tino.calancha@gmail.com>
2019-07-30 19:16:45 +0200

    * gited-tests.el: Update copyright notice

--- gited-tests.el
+++ gited-tests.el
@@ -1,6 +1,6 @@
 ;;; gited-tests.el --- Tests for gited.el  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2017 Free Software Foundation, Inc.
+;; Copyright (C) 2017-2019 Free Software Foundation, Inc.
 
 ;; Author: Tino Calancha <tino.calancha@gmail.com>,
 ;; Keywords:
